3d70f2e092a6be674f265d87c0082b5a2dcf97aa
Stephen Anderson <stephenanderson@Stephens-MacBook-Air.local>
2017-07-28 15:46:35 -0700

    Add placeholder for directions route

--- src/routes/directions.js
+++ src/routes/directions.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 
 router.get('/directions', (req, res) => {
-res.status(200).send(`origin: ${req.query.origin} <br/> distination: ${req.query.destination} <br/> time: ${req.query.time}`)
+  res.status(200).send(`origin: ${req.query.origin} <br/> distination: ${req.query.destination} <br/> time: ${req.query.time}`)
 })
+
+module.exports = router;
