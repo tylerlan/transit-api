b8c854a0536d05ee39dbc71ac5669ac85278d93c
Kurtis Houser <kurtishouser@users.noreply.github.com>
2017-08-03 10:36:56 -0700

    Add cors package (#6)
    * Add cors package
    
    * Remove ^ from package.json

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const directions = require('./src/routes/directions');
 
